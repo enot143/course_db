094f0ea48b69fec6d7648c8d484cbe57d59cda37
enot143 <arina.stenina@mail.ru>
2021-11-19 13:02:01 +0300

    cors

--- src/main/java/db/course/config/WebSecurityConfig.java
+++ src/main/java/db/course/config/WebSecurityConfig.java
@@ -46,9 +46,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-                .configurationSource(corsConfigurationSource())
-                .and()
+        http
+//                .cors()
+//                .configurationSource(CorsConfig)
+//                .and()
                 .csrf()
                 .disable()
                 .authorizeRequests()
